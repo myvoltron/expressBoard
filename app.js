d26775dc63a42decc5f94b3337f6d0b55d4e284b
myvoltron <leejs8266@gmail.com>
2022-03-12 19:06:22 +0900

    app.js 및 header.ejs 수정

--- app.js
+++ app.js
@@ -77,7 +77,8 @@ app.get('/', (req, res) => {
 });
 app.use((req, res, next) => {
 
-    res.locals.user = req.user !== null && req.user !== undefined ? req.user : null; // user를 전역적으로 처리하게 함 
+    // console.log(req.user);
+    res.locals.user = req.user !== null && req.user !== undefined ? req.user : undefined; // user를 전역적으로 처리하게 함 
     next();
 }); 
 app.use('/post', postRouter); 
